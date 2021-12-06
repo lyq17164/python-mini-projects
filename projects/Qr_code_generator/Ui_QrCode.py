1b31001ab9607dcaf8ca20d77238af34437913d5
lyq17164 <1405432459@qq.com>
2021-12-06 22:02:53 +0800

    QrCode Version 1.0

---
+++ projects/Qr_code_generator/Ui_QrCode.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'f:\Project\python-mini-projects\projects\Qr_code_generator\QrCode.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(554, 315)
+        Form.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setMinimumSize(QtCore.QSize(0, 0))
+        self.label.setMaximumSize(QtCore.QSize(400, 400))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.edit = QtWidgets.QTextEdit(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.edit.sizePolicy().hasHeightForWidth())
+        self.edit.setSizePolicy(sizePolicy)
+        self.edit.setMinimumSize(QtCore.QSize(0, 0))
+        self.edit.setObjectName("edit")
+        self.horizontalLayout.addWidget(self.edit)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.btn = QtWidgets.QPushButton(Form)
+        self.btn.setObjectName("btn")
+        self.verticalLayout.addWidget(self.btn)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "二维码生成"))
+        self.btn.setText(_translate("Form", "PushButton"))
+
---
+++ projects/Qr_code_generator/main.py
@@ -0,0 +1,41 @@
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from Ui_QrCode import *
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+import qrcode
+from PIL.ImageQt import ImageQt
+from io import BytesIO
+class MyForm(QWidget, Ui_Form):
+    def __init__(self):    
+        super(MyForm, self).__init__()
+        self.setupUi(self)
+        self.resize(400, 300)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.btn.clicked.connect(self.btnstate)
+        self.label.setScaledContents (True)
+    
+    def btnstate(self):
+        str = self.edit.toPlainText()
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=6,
+            border=1,
+        )
+        qr.add_data(str)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="red", back_color="white")
+        buf = BytesIO()
+        img.save(buf,"PNG")
+        qt_pixmap = QPixmap()
+        qt_pixmap.loadFromData(buf.getvalue(),"PNG")
+        
+        self.label.setPixmap(qt_pixmap)
+        
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = MyForm()
+    form.show()
+    sys.exit(app.exec_())
